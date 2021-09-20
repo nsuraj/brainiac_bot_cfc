91e3cf27c5e2a2775301e69a7592cb25b3b004ea
suraj <nsurajrajan@gmail.com>
2021-09-20 11:18:37 +0530

    adding files

---
+++ src/app/cov19desc/cov19desc.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cov19desc',
+  templateUrl: './cov19desc.component.html',
+  styleUrls: ['./cov19desc.component.css']
+})
+export class Cov19descComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
